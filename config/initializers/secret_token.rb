bfe9f13339ecdadaba9b2f00491666f5ce38ee10
F4x <spirit2196@gmail.com>
2013-05-25 14:51:02 +0400

    Start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SamleApp::Application.config.secret_token = 'ab1ec405f5f56e3737959f2144b8f2b6599eeab7f1e780f05619650342e6f13b0cedfa2bca13a14d2f23fc861beb37510752da7590f6dacfd5a6b97c04b3c142'
